8b37245444ec59a4c0265e6505da6923552e180c
Bob Stockdale <robert.stockdale@gmail.com>
2020-10-30 07:56:11 -0400

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+
+export interface CdkShoestringDockerEcsAppProps {
+  // Define construct properties here
+}
+
+export class CdkShoestringDockerEcsApp extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: CdkShoestringDockerEcsAppProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
