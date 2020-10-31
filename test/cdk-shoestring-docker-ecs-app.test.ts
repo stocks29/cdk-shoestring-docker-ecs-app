29a865c7d8e4bafb0b6b3a62321dbe97d0100734
Bob Stockdale <robert.stockdale@gmail.com>
2020-10-31 00:16:00 -0400

    fix failing test

--- test/cdk-shoestring-docker-ecs-app.test.ts
+++ test/cdk-shoestring-docker-ecs-app.test.ts
@@ -5,7 +5,7 @@ import * as shoestring from '../lib/index';
 /*
  * Example test 
  */
-test('ECS Service Created', () => {
+test('ECS Service not created', () => {
   const app = new cdk.App();
   const stack = new cdk.Stack(app, "TestStack");
   // WHEN
@@ -13,6 +13,30 @@ test('ECS Service Created', () => {
     codeRepositoryName: 'commitRepo',
     ecrRepositoryName: 'ecrRepo',
     region: 'us-east-1',
+    environments: [
+      {
+        appPort: 4000,
+        envVariables: {
+          PORT: '4000',
+        },
+        name: 'Prod',
+      }
+    ]
+  });
+  // THEN
+  expectCDK(stack).to(countResources("AWS::ECS::Service",0));
+});
+
+
+test('ECS Service created', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
+  // WHEN
+  new shoestring.CdkShoestringDockerEcsApp(stack, 'MyShoestringStartupApp', {
+    codeRepositoryName: 'commitRepo',
+    ecrRepositoryName: 'ecrRepo',
+    region: 'us-east-1',
+    setupServices: true,
     environments: [
       {
         appPort: 4000,
